dc1e338dbf7bd7c6ffb466f2211ba13e64dc0e75
Nicholas Gareth Jennings <nichol462@gmail.com>
2023-05-18 15:27:10 +0200

    fixed 2.1

--- Assignment_3/exercise_2.py
+++ Assignment_3/exercise_2.py
@@ -1,34 +1,5 @@
 # TODO: Add any imports you deem necessary
 
-def get_encoding_old(alphabet: dict, base: int = 2) -> dict:
-    """ Generate an prefix encoding for each element of the alphabet
-
-    Args:
-    alphabet - a dictionary that holds in its keys the alphabet item and
-                   its frequency as the value, e.g. {'a': 0.5, 'b': 0.5}
-    base - an `int` that represents the base of the code. Feel free to make
-           your implementation open to other bases. For the purposes of this
-           exercise, using only base `2`, i.e. binary code, suffices.
-
-    Returns a `dict` with alphabet items in its keys and the encoding as the
-    values, e.g. {'a': '0', 'b': '1'}
-    """
-    counter = 0
-    code_dict = dict()
-    encoding_set = set()
-    alphabet_list = list(alphabet.items())
-    alphabet_list = sorted(alphabet_list, key=lambda x: x[1])
-    for i, (a, p) in enumerate(alphabet_list):
-        if i == len(alphabet_list) - 1:
-            code_dict[a] = toBase(counter - 1)[2:]  # sliced the '0b' prefix off the bin() numbers
-            return code_dict
-        while has_prefix(toBase(counter), encoding_set):
-            counter += 1
-        code_dict[a] = toBase(counter)[2:]  # sliced the '0b' prefix off the bin() numbers
-        encoding_set.add(toBase(counter))
-        counter += base
-    return code_dict  # added final return case to fix a linter error
-
 
 def get_encoding(alphabet: dict, base: int = 2) -> dict:
     """ Generate an prefix encoding for each element of the alphabet
@@ -46,30 +17,33 @@ def get_encoding(alphabet: dict, base: int = 2) -> dict:
     alphabet_list = list(alphabet.items())
     alphabet_list = sorted(alphabet_list, key=lambda x: x[1], reverse=True)
     code_dict = dict()
-    tree = split_list_shannon(alphabet_list)
+    tree = shannon(alphabet_list)
     for alpha, code in flatten(tree):
         code_dict[alpha] = code
     return code_dict
 
 
-def split_list(lst, parts):
-    length = len(lst)
-    return [lst[i * length // parts: (i + 1) * length // parts] for i in range(parts)]
+def split_list(lst):
+    lst_branch = []
+    while sum(x[1] for x in lst_branch) < sum(x[1] for x in lst):  # and
+        branch_sum = sum(x[1] for x in lst_branch)
+        lst_sum = sum(x[1] for x in lst)
+        lst_branch.append(lst.pop(0))
+        branch_sum_new = sum(x[1] for x in lst_branch)
+        lst_sum_new = sum(x[1] for x in lst)
+        if abs(branch_sum_new - lst_sum_new) > abs(branch_sum - lst_sum):
+            # revert lists
+            lst = [lst_branch.pop(-1)] + lst
+            break
+    return lst_branch, lst
 
 
-def split_list_shannon(lst, code=""):
-    print(lst)
+def shannon(lst, code=""):
     if len(lst) == 1:
         return lst[0][0], code
-    lst_branch = []
-    while sum(x[1] for x in lst_branch) + lst[0][1] <= sum(x[1] for x in lst) - lst[0][1]:
-        lst_branch.append(lst.pop(0))
-        if len(lst) == 0:
-            print("lst == 0")
-            head = lst_branch.pop(0)
-            return [split_list_shannon([head], code=code + "0"), split_list_shannon(lst, code=code + "1")]
-    return [split_list_shannon(lst_branch, code=code + "0"),
-            split_list_shannon(lst, code=code + "1")]
+    split_lst = split_list(lst)
+    return [shannon(split_lst[0], code=code + "0"),
+            shannon(split_lst[1], code=code + "1")]
 
 
 def flatten(lst):
@@ -80,14 +54,3 @@ def flatten(lst):
                 yield nested_item
         else:
             yield item
-
-
-def has_prefix(string, strings):
-    for s in strings:
-        if string.startswith(s):
-            return True
-    return False
-
-
-def toBase(x, base=2):
-    return bin(x)
