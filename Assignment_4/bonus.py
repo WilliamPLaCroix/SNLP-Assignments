ca5b53d206583330b20e2bba73a0bd2f105b459d
Nicholas Gareth Jennings <nichol462@gmail.com>
2023-05-25 19:34:58 +0200

    update bonus

--- Assignment_4/bonus.py
+++ Assignment_4/bonus.py
@@ -1,6 +1,19 @@
+from transformers import GPT2Tokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+
 def char_tokenize(corpus):
     char_corpus = []
     for sent in corpus:
         sent_tokenized = [c for c in " ".join(sent)]
         char_corpus.append(sent_tokenized)
     return char_corpus
+
+
+def byte_pair_tokenize(corpus):
+    byte_pair_corpus = []
+    for sent in corpus:
+        sent_tokenized = [str(token) for token in tokenizer(" ".join(sent))["input_ids"]]
+        byte_pair_corpus.append(sent_tokenized)
+    return byte_pair_corpus
