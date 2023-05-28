f21c0b22fd2b348bb1345e8f7537ce254fee9d9b
WilliamPLaCroix <williamplacroix@gmail.com>
2023-05-28 14:27:28 +0200

    fixed a few linter errors

--- Assignment_5/exercise_1.py
+++ Assignment_5/exercise_1.py
@@ -1,24 +1,30 @@
-from collections import defaultdict,Counter
-import nltk
 import string
+from collections import Counter, defaultdict
+
+import nltk
+
 nltk.download('treebank')
 
 def load_and_preprocess_data():
-  corpus=list(nltk.corpus.treebank.sents())
-  cleaned_corpus=[]
-  for sent in corpus:
+    corpus=list(nltk.corpus.treebank.sents())
+    cleaned_corpus=[]
+    for sent in corpus:
     #Remove punctuations
-    words = [word.translate(str.maketrans(dict.fromkeys(string.punctuation))).lower() for word in sent]
-    #Remove empty tokens after cleaning
-    words=" ".join(words).split()
-    if len(words)>=2:
-      cleaned_corpus.append(words)
-  return cleaned_corpus
+        words = [word.translate(str.maketrans(dict.fromkeys(string.punctuation))).lower() for word in sent]
+        #Remove empty tokens after cleaning
+        words = " ".join(words).split()
+        if len(words)>=2:
+            cleaned_corpus.append(words)
+    return cleaned_corpus
 
-  class Interpolated_Model():
-    #Complete this class
-    def _init_(args):
-      raise NotImplementedError
+class Interpolated_Model():
+    """#TODO"""
+#Complete this class
+    def _init_(self, args):
+        """#TODO"""
+        raise NotImplementedError
 
-    def perplexity(args):
-      raise NotImplementedError
+    def perplexity(self, args):
+        """#TODO"""
+        raise NotImplementedError
+    
