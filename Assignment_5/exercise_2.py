fad18b6f2b60f5415cf21e202b726cd583ebe6f4
WilliamPLaCroix <williamplacroix@gmail.com>
2023-05-30 16:31:27 +0200

    deleted example

--- Assignment_5/exercise_2.py
+++ Assignment_5/exercise_2.py
@@ -5,18 +5,11 @@ def preprocess_text(text: str) -> "list[str]":
     """
     Preprocesses the given text by removing punctuation, converting to lowercase,
     and splitting it into tokens.
-
     Args:
         text (str): The input text to be preprocessed.
-
     Returns:
         list[str]: A list of tokens obtained after removing punctuation, converting
             to lowercase, and splitting the input text.
-
-    Example:
-        >>> text = "Hello, World!"
-        >>> preprocess_text(text)
-        ['hello', 'world']
     """
     return text.translate(str.maketrans('', '', string.punctuation)).lower().split()
 
