56d099e83a48d1a40bd4a01a6efe26c1f9cd5e6f
WilliamPLaCroix <williamplacroix@gmail.com>
2023-05-30 12:24:17 +0200

    I lied, no oov file...

--- Assignment_5/exercise_1.py
+++ Assignment_5/exercise_1.py
@@ -1,7 +1,8 @@
 import string
-from collections import Counter, defaultdict
+from collections import Counter  # defaultdict
 
 import nltk
+#import numpy as np
 from nltk import ngrams
 
 # nltk.download('treebank') # Uncomment this line if you have not downloaded the Treebank corpus
@@ -9,15 +10,12 @@ from nltk import ngrams
 def load_and_preprocess_data() -> "list[list[str]]":
     """
     Loads and preprocesses the data from the NLTK Treebank corpus.
-
+    Args:
+        None
     Returns:
         List[List[str]]: A list of lists, where each inner list represents a sentence
             from the Treebank corpus after removing punctuation, converting to lowercase,
             and removing empty tokens.
-
-    Example:
-        >>> load_and_preprocess_data()
-        [['this', 'is', 'an', 'example', 'sentence'], ['another', 'example', 'sentence']]
     """
     # Remove punctuation and lowercase, removing empty tokens after cleaning
     # for sent in sents: # Remove sentences with only one word. Not sure if we'll need this.
@@ -25,6 +23,49 @@ def load_and_preprocess_data() -> "list[list[str]]":
     #         corpus.remove(sent)
     return [" ".join(sent).lower().translate(str.maketrans('', '', string.punctuation)).split() 
             for sent in list(nltk.corpus.treebank.sents())]
+
+def make_vocab(corpus: "list[list[str]]", top_n: int) -> "list[str]":
+    """
+    Creates a vocabulary of the most frequent words in the given corpus.
+    Args:
+        corpus (list[list[str]]): A list of lists, where each inner list represents a sentence
+            in the corpus.
+        top_n (int): The number of words to include in the vocabulary.
+    Returns:
+        List[str]: The vocabulary, which is a list of the most frequent words in the corpus.
+    """
+    vocab = dict()
+    for sent in corpus:
+        for word in sent:
+            if word in vocab:
+                vocab[word] += 1
+            else:
+                vocab[word] = 1
+    vocab_sorted = sorted(list(vocab.items()), key=lambda x: x[1], reverse=True)[:top_n]
+
+    return [v[0] for v in vocab_sorted]
+
+def restrict_vocab(corpus: "list[list[str]]") -> "list[list[str]]":
+    """
+    Restricts the vocabulary of the given corpus to a specified size.
+    Args:
+        corpus (list[list[str]]): A list of lists, where each inner list represents a sentence
+            in the corpus.
+    Returns:
+        list[list[str]]: The restricted corpus, where words not present in the vocabulary are
+            replaced with "<unk>".
+    """
+    vocab: "list[str]" = make_vocab(corpus, 5000)
+    corpus_restrict: "list[list[str]]" = []
+    for sent in corpus:
+        sent_restrict: "list[str]" = []
+        for word in sent:
+            if word not in vocab:
+                sent_restrict.append("<unk>")
+            else:
+                sent_restrict.append(word)
+        corpus_restrict.append(sent_restrict)
+    return corpus_restrict
 
 # use a 70:30 split for train/test. Take test from the last 30%. Do not randomize at this stage.
 def split_corpus(corpus: "list[list[str]]"):
@@ -36,6 +77,7 @@ def split_corpus(corpus: "list[list[str]]"):
         train: (list[list[str]]), test: (list[list[str]]) : A tuple containing the train and test set splits.
     """
     return corpus[:int(len(corpus)*0.7)], corpus[int(len(corpus)*0.7):]
+
 
 class InterpolatedModel():
     """#TODO"""
@@ -72,7 +114,7 @@ class InterpolatedModel():
             ngram_counts.update(list(ngrams(sent, order)))
         return Counter(dict(ngram_counts.most_common()))
 
-    def set_ngram_counts(self, order) -> "tuple[list[dict[tuple[str, ...], int]], list[dict[tuple[str, ...], int]]]":
+    def set_ngram_counts(self, order: int) -> "tuple[list[dict[tuple[str, ...], int]], list[dict[tuple[str, ...], int]]]":
         """
         Sets the n-gram counts for training and test data for each n up to the given 'order'.
         Args:
@@ -85,16 +127,12 @@ class InterpolatedModel():
         Example:
             >>> model = InterpolatedModel()
             >>> model.set_ngram_counts(3)
-            ([{unigram_counts}, {bigram_counts}, {trigram_counts}], [{unigram_counts}, {bigram_counts}, {trigram_counts}])
+            (train[{unigram_counts}, {bigram_counts}, {trigram_counts}], test[{unigram_counts}, {bigram_counts}, {trigram_counts}])
         """
-        train_counts = list()
-        test_counts = list()
-        for i in range(order):
-            train_counts.append(self.ngram_counter(self.train, i))
-            test_counts.append(self.ngram_counter(self.test, i))
-        return train_counts, test_counts
+        return ([self.ngram_counter(self.train, i) for i in range(order)],
+                 [self.ngram_counter(self.test, i) for i in range(order)])
 
-    def logprob(self, bigram: tuple) -> float:
+    def logprob(self, bigram: "tuple[str,...]") -> float:
         """#TODO"""
         # PLaplace(w_n|w_n-1) = (C(w_n-1,w_n) + alpha)
         #                       (C(w_n-1) + alpha * V)
@@ -102,9 +140,10 @@ class InterpolatedModel():
         # numerator = self.train_bigram_counts[bigram] + self.alpha
         # denominator = self.train_unigram_counts[(bigram[0],)] + self.alpha * len(self.train_unigram_counts)
         # try:
-        #     return log(numerator / denominator)
+        #     return np.log(numerator / denominator)
         # except ValueError:
         #     return -100000000000000000000000000000000
+        raise NotImplementedError
         
     def perplexity(self, order: int) -> float:
         """#TODO"""
@@ -115,4 +154,5 @@ class InterpolatedModel():
         # return np.exp(-sum(
         #     [(self.test_bigram_counts[bigram] / N) * self.logprob(bigram)
         #         for bigram in self.test_bigram_counts]))
+        raise NotImplementedError
     
