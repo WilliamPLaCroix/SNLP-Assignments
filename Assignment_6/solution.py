1d23683564e0eccbba82f2adbbf31cf5a8ab0dd7
WilliamPLaCroix <williamplacroix@gmail.com>
2023-06-02 12:44:45 +0200

    uploaded black assignment 6

---
+++ Assignment_6/solution.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Do any necessary imports here
+
+def load_and_preprocess_data(path: str, remove_punct: bool, lowercase: bool) -> list:
+  ''' Function that loads the FinancialPhraseBank dataset, removes punctuation and lowercases the tokens based on arguments'''
+  raise NotImplementedError
+
+# Define your own functions, ideally each step should have their corresponding function
