735d8e30b0b81e2aee85407de328062ba8d947ea
WilliamPLaCroix <williamplacroix@gmail.com>
2023-06-05 19:10:03 +0200

    fixed corpus loading

--- Assignment_6/solution.py
+++ Assignment_6/solution.py
@@ -3,32 +3,24 @@
 import string
 
 import nltk
+import pandas as pd
 
 
-#TODO this doesn't work yet, the CSV has tags and sentences. We need to split them up
-# and then do the preprocessing on the sentences to return a list of lists of words, not letters
 def load_and_preprocess_data(path: str, remove_punct: bool, lowercase: bool) -> "list[list[str]]":
-    """
-    Loads and preprocesses the data from the NLTK Treebank corpus.
-    Args:
-        None
-    Returns:
-        List[List[str]]: A list of lists, where each inner list represents a sentence
-            from the Treebank corpus after removing punctuation, converting to lowercase,
-            and removing empty tokens.
-    """
-    # Remove punctuation and lowercase, removing empty tokens after cleaning
+    """#TODO """
 
-    with open(path, 'r', encoding="ISO-8859-1") as file_in:
-        data = file_in.readlines()
+    dataframe = pd.read_csv(path, encoding="ISO-8859-1", header=None)
+    data = dataframe[1].tolist()
     corpus = list()
     for sent in data:
+        sent = sent.split()
         if remove_punct:
             sent = [word.translate(str.maketrans('', '', string.punctuation)) for word in sent]
         if lowercase:
             sent = [word.lower() for word in sent]
-        corpus.append(" ".join(sent).split())
-    return corpus
+        corpus.append(" ".join(" ".join(sent).split()))
+    dataframe[1] = corpus
+    return dataframe
 
 def train_test_split(corpus: "list[list[str]]", train_ratio: float = 0.8) -> "tuple[list[list[str]], list[list[str]]]":
     """
