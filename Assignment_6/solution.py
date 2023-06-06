d7150d8556f46ecb2b93ddc5e245f65ac0d8b6d7
WilliamPLaCroix <williamplacroix@gmail.com>
2023-06-06 18:12:06 +0200

    XGBoosted

--- Assignment_6/solution.py
+++ Assignment_6/solution.py
@@ -9,7 +9,7 @@ import sklearn.metrics
 from numpy import ndarray
 
 
-def confusion_matrix(y_true: "list[int]", y_pred: "list[int]|ndarray") -> "ndarray":
+def confusion_matrix(y_true: "list[int]", y_pred: "list[int]|ndarray"): # -> "ndarray":
     """
     Create a confusion matrix from two lists of labels.
     Args:
@@ -18,7 +18,8 @@ def confusion_matrix(y_true: "list[int]", y_pred: "list[int]|ndarray") -> "ndarr
     Returns:
         ndarray: The confusion matrix.
     """
-    return sklearn.metrics.confusion_matrix(y_true, y_pred)
+    return pd.crosstab(y_true, y_pred, rownames=['True'], colnames=['Predicted'], margins=True)
+    # return sklearn.metrics.confusion_matrix(y_true, y_pred)
 
 
 def load_and_preprocess_data(path: str, remove_punct: bool, lowercase: bool) -> "list[list[str]]":
@@ -42,6 +43,33 @@ def vectorize_set(corpus: "list[list[str]]") -> "tuple[ndarray[spmatrix]": # typ
     vectorizer = sklfe.CountVectorizer()
     return vectorizer.fit_transform(corpus).toarray() # type: ignore
  
+def test(confusion_matrix: "pd.DataFrame", classifier: str) -> None:
+
+    # Accuracy = (TP + TN) / (TP + TN + FP + FN)
+    # Precision = TP / (TP + FP)
+    # Recall = TP / (TP + FN)
+    # F1 = 2 * (Precision * Recall) / (Precision + Recall)
+
+    if classifier == 'XGBoost':
+        TP = confusion_matrix[2][2]
+        TN = confusion_matrix[0][0]
+        FP = confusion_matrix[0][2] + confusion_matrix[1][2]
+        FN = confusion_matrix[1][0] + confusion_matrix[2][0]
+    else: # classifier == 'Naive Bayes':
+        TP = confusion_matrix['positive']['positive']
+        TN = confusion_matrix['negative']['negative']
+        FP = confusion_matrix['negative']['positive'] + confusion_matrix['neutral']['positive']
+        FN = confusion_matrix['neutral']['negative'] + confusion_matrix['positive']['negative']
+
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    print(f"\n{classifier} accuracy:", accuracy)
+    precision = TP / (TP + FP)
+    print(f"{classifier} precision:", precision)
+    recall = TP / (TP + FN)
+    print(f"{classifier} recall:", recall)
+    f1 = 2 * (precision * recall) / (precision + recall)
+    print(f"{classifier} F1:", f1)
+    return None
 
 def train_test_split(corpus: "list[list[str]]", train_ratio: float = 0.8) -> "tuple[list[list[str]], list[list[str]]]":
     """
